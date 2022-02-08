53c9622f3669f808370c17252b48002e45921b1f
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-08 13:52:23 +0900

    initial commit

--- src/abstractex/AbstractEx2.java
+++ src/abstractex/AbstractEx2.java
@@ -1,9 +1,50 @@
 package abstractex;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+abstract class Shape{ //다른 패키지에 있는 이름 중복돼도 됨.
+	abstract public void OnDraw();
+	abstract public void OnDelete();
+}
+class Rectangle extends Shape{
+	@Override
+	public void OnDraw() {
+		System.out.println("사각형을 그려요...");
+	}
+	@Override
+	public void OnDelete() {
+		System.out.println("사각형을 지워요...");
+}
+}
+	class Triangle extends Shape{
+		@Override
+		public void OnDraw() {
+			System.out.println("삼각형을 그려요...");
+		}
+		@Override
+		public void OnDelete() {
+			System.out.println("삼각형을 지워요...");
+	}
+	}
 public class AbstractEx2 {
 
 	public static void main(String[] args) {
-
+		Shape sh=null;
+		sh=new Rectangle();
+		sh.OnDraw();
+		sh.OnDelete();
+		
+		sh= new Triangle();
+		sh.OnDraw();
+		sh.OnDelete();
+		
+		Rectangle rt=new Rectangle();
+		rt.OnDraw();
+		rt.OnDelete();
+		
+		Triangle tr=new Triangle();
+		tr.OnDraw();
+		tr.OnDelete();
 	}
 
 }
